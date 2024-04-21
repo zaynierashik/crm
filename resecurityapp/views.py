d78884c707a24754940da67cc3fff59391342270
User <zaynierashik@gmail.com>
2024-04-21 16:17:21 +0545

    Completed

--- resecurityapp/views.py
+++ resecurityapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
